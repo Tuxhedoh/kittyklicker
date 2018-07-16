ad049ca5a515308f35dc97d5d9a97963899e614f
Tuxhedoh <deanmck@gmail.com>
2018-07-16 11:03:40 -0400

    Two kats visible, not clickable yet

--- app/app.js
+++ app/app.js
@@ -1,11 +1,37 @@
-const myScore = document.querySelector('#theScore'),
-    myImage = document.querySelector('#kitty1');
-let currentScore = 0;
+// Version 2 requires 2 cats, each having a name and a score.
+// I think I'm going to objectify my cats!
 
-myImage.addEventListener('click', katKlicked);
+function Kat(name){
+    this.name= name,
+    this.score= 0;
+    this.pic='/app/images/hairless.jpg'
+    this.click = function(){
+        return ++this.score;
+    }
 
-function katKlicked(){
-    currentScore++;
-    myScore.innerText = currentScore;    
-}
+};
+
+const body = document.querySelector('.main');
+
+let kats = [];
+kats.push(kat1 = new Kat('whiskers'));
+kats.push(kat2 = new Kat('polly'));
+
+// loop through kats. 
+kats.forEach(function(kat){
+
+    // create individual Kat Div
+    var thisKatDiv = document.createElement('div');
+    
+    // create HTML using String Template
+    var katHTML = 
+   `<div class="katName">${kat.name}</div>
+    <div class="katScore">${kat.score}</div>
+    <div class="katPic"><img src="${kat.pic}" alt="a kat pic"></img></div>`;
+
+    
+    thisKatDiv.innerHTML= katHTML;
+
+    body.appendChild(thisKatDiv);
+});
 
